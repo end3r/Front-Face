3bad179251bdcdfd752a4101951192c4671343e2
Andrzej Mazur <andrzej.mazur@end3r.com>
2012-03-22 01:10:01 +0100

    fixed two annoying bugs with duplicated answers (now 100% working), added some html5 apis: offline, local storage, geolocation

--- js/game.js
+++ js/game.js
@@ -7,6 +7,7 @@ GAME.Init = function() {
 	GAME.$id('howto').onclick = function() { GAME.Page('howto'); };
 	GAME.$id('about').onclick = function() { GAME.Page('about'); };
 	GAME._counter = 10;
+	//GAME.API.geolocation();
 };
 
 GAME.Start = function() {
@@ -21,13 +22,6 @@ GAME.Start = function() {
 };
 
 GAME.NewLevel = function(lvl) {
-	GAME._board = [];
-	// take the items from the data table and add them twice
-	for(var i = 0; i < GAME._counter; i++) {
-		var data = GAME.data[((lvl-1)*GAME._counter)+i];
-		GAME._board.push(data, data);
-	}
-
 	// generate HTML structure of the game board
 	var board = GAME.$id('board');
 	for (var i = 0, elements = ''; i < GAME._counter*2; i++) {
@@ -41,6 +35,13 @@ GAME.NewLevel = function(lvl) {
 		els[i].onclick = function() { GAME.CardClick(this); };
 	}
 
+	GAME._board = [];
+	// take the items from the data table and add them twice
+	for(var i = 0; i < GAME._counter; i++) {
+		var data = GAME.data[((lvl-1)*GAME._counter)+i];
+		GAME._board.push(data, data);
+	}
+
 	// shuffle array 10 times - just to be sure it's more or less random
 	for (var i = 10; i >= 0; i--) {
 		GAME.shuffleArray(GAME._board);
@@ -49,15 +50,13 @@ GAME.NewLevel = function(lvl) {
 
 GAME.HideCards = function() {
 	var item1 = GAME._visible.pop(),
-		item2 = GAME._visible.pop();
-
-	var myItem1 = GAME.$id('card_'+item1.card_id);
+		item2 = GAME._visible.pop(),
+		myItem1 = GAME.$id('card_'+item1.card_id),
+		myItem2 = GAME.$id('card_'+item2.card_id);
 	myItem1.className = '';
 	myItem1.style.backgroundPosition = '';
-	var myItem2 = GAME.$id('card_'+item2.card_id);
 	myItem2.className = '';
 	myItem2.style.backgroundPosition = '';
-
 	GAME._active = true;
 	myItem1.onclick = function() { GAME.CardClick(this); };
 	myItem2.onclick = function() { GAME.CardClick(this); };
@@ -65,21 +64,17 @@ GAME.HideCards = function() {
 
 GAME.DisableCards = function() {
 	var item1 = GAME._visible.pop(),
-		item2 = GAME._visible.pop();
-
-	var myItem1 = GAME.$id('card_'+item1.card_id);
+		item2 = GAME._visible.pop(),
+		myItem1 = GAME.$id('card_'+item1.card_id),
+		myItem2 = GAME.$id('card_'+item2.card_id);
 	myItem1.className = 'disabled';
 	myItem1.onclick = function() {};
-
-	var myItem2 = GAME.$id('card_'+item2.card_id);
 	myItem2.className = 'disabled';
 	myItem2.onclick = function() {};
-
 	GAME._active = true;
 };
 
 GAME.CardClick = function(card) {
-
 	if(GAME._active) {
 		var card_id = card.id.split('_')[1],
 			item = GAME._board[card_id];
@@ -91,9 +86,9 @@ GAME.CardClick = function(card) {
 
 		if(GAME._visible.length == 2) { // two visible cards
 			if(GAME._visible[0].item_id == GAME._visible[1].item_id) { // the same card ID
-				// ask question - if answered correct, add points, if not - don't
-				GAME.Form(item);
+				// show the form with the question
 				setTimeout(GAME.DisableCards,1);
+				GAME.Form(item);
 			}
 			else {
 				setTimeout(GAME.HideCards,1000);
@@ -109,56 +104,39 @@ GAME.Form = function(item) {
 	var question = {
 		txt: ['name','website','Twitter username','topic of the talk'],
 		id: ['name','website','twitter','topic'],
-		nr: Math.floor(Math.random()*4),
+		nr: -1,
 		tab: []
 	};
 
-	// get 3 different values - the proper one and two random
 	do {
-		var subject = question.id[question.nr]; // fuk
-		//console.log('SUBJECT: '+subject);
+		question.nr = ~~(Math.random()*4);
+		var subject = question.id[question.nr];
 	} while(item[subject] == '');
 	
 	var answerTable = [];
 
-	//console.log('item[subject]: '+item[subject]);
-
-	// proper
+	// push one proper answer...
 	answerTable.push(item[subject]);
 
-	// random
-	for(var i=0; i<2; i++) {
-		var newRandom = false;
-		do {
-			var random = Math.floor(Math.random()*(GAME._board.length/2));
-			for (var j = 0; j < answerTable.length; j++) {
-				if(GAME.data[random][subject] != '' && GAME.data[random][subject] != answerTable[j]) {
-					newRandom = true;
-					//console.log('newRandom: '+GAME.data[random][subject]);
-				}
-				else {
-					;//console.log('newRandom... NOT: '+GAME.data[random][subject]);
+	// ...and get the two different random answers
+	do {
+		var randomAnswer = GAME.data[~~(Math.random()*GAME.data.length)][subject];
+		if(randomAnswer) {
+			var inUse = false;
+			for(var i = 0; i < answerTable.length; i++) {
+				if(answerTable[i] == randomAnswer) {
+					inUse = true;
 				}
 			}
-		} while(newRandom == false);
-		answerTable.push(GAME.data[random][subject]);
-	}
+			if(!inUse) {
+				answerTable.push(randomAnswer);
+			}
+		}
+	} while(answerTable.length < 3);
+
 	GAME.shuffleArray(answerTable);
 
-	var formHTML = ""+
-	"<div class='modal'>"+
-		"<div class='header'>"+
-			"<span class='photo' style='background-position: -"+(item.id*100)+"px 0;'></span>"+
-			"<span id='message'>What's the <strong>"+question.txt[question.nr]+"</strong> of this person?</span>"+
-		"</div>"+
-		"<form id='form'>"+
-			"<p><input type='radio' name='q' value='"+answerTable[0]+"' id='radio1' /> <label id='radio1label' for='radio1'>"+answerTable[0]+"</label></p>"+
-			"<p><input type='radio' name='q' value='"+answerTable[1]+"' id='radio2' /> <label id='radio2label' for='radio2'>"+answerTable[1]+"</label></p>"+
-			"<p><input type='radio' name='q' value='"+answerTable[2]+"' id='radio3' /> <label id='radio3label' for='radio3'>"+answerTable[2]+"</label></p>"+
-		"</form>"+
-		"<div class='continue'><span id='continue'>Continue</span></div>"+
-	"</div>";
-
+	var formHTML = GAME.$form(item,question.txt[question.nr],answerTable);
 	GAME.$showModal(formHTML);
 	GAME.$id('radio1').onclick = function() { GAME.CheckAnswer(this,item[subject]); };
 	GAME.$id('radio2').onclick = function() { GAME.CheckAnswer(this,item[subject]); };
@@ -168,7 +146,7 @@ GAME.Form = function(item) {
 
 GAME.CheckAnswer = function(chosen,correct) {
 	if(chosen.value == correct) {
-		GAME.$id('message').innerHTML = 'Congratulations, your answer is correct!';
+		GAME.$id('message').innerHTML = GAME.$txt.correct;
 		chosen.style.background = 'green';
 		GAME._points += 10;
 		GAME.$id('points').innerHTML = GAME._points;
@@ -176,7 +154,7 @@ GAME.CheckAnswer = function(chosen,correct) {
 		GAME.$id(chosen.id+'label').innerHTML;// += ' ✓';
 	}
 	else {
-		GAME.$id('message').innerHTML = 'Oh no, wrong answer, no points this time...';
+		GAME.$id('message').innerHTML = GAME.$txt.wrong;
 		GAME.$id(chosen.id+'label').style.color = 'red';
 		GAME.$id(chosen.id+'label').innerHTML;// += ' ✗';
 	}
@@ -184,28 +162,17 @@ GAME.CheckAnswer = function(chosen,correct) {
 	GAME._questions += 1;
 	GAME.$id('questions').innerHTML = GAME._questions;
 	GAME.$id('continue').style.visibility = 'visible';
-
 	GAME.$id('radio1').disabled = true;
 	GAME.$id('radio2').disabled = true;
 	GAME.$id('radio3').disabled = true;
-
-//	GAME.$id('radio1label').style.color = 'red';
-//	GAME.$id('radio2label').style.color = 'red';
-//	GAME.$id('radio3label').style.color = 'red';
-
 	GAME.$id('form').className = 'disabled';
-
 //	GAME.$id(correct.id+'label').style.color = 'green';
 
 	GAME.$id('continue').onclick = function() {
 		GAME.$hideModal();
-
 		if(GAME._questions == 10) {
 			setTimeout(function(){
-				GAME.$showModal("<div class='modal'>"+
-					"<div>Nice, You're half way thru! You've managed to solve the first 10 pairs of photos, let's see how You'll handle the second part of the game.</div>"+
-					"<div class='continue'><span id='newLevel'>Continue</span></div>"+
-				"</div>");
+				GAME.$showModal(GAME.$txt.halfway+GAME.$txt.continue);
 				GAME.$id('newLevel').style.visibility = 'visible';
 				GAME.$id('newLevel').onclick = function() {
 					GAME.$hideModal();
@@ -238,15 +205,29 @@ GAME.TimerStop = function() {
 
 GAME.Page = function(page) {
 	if(page == 'gameover') {
+
+		// HTML5 OFFLINE
+		if (navigator.onLine) {
+			// serve normal stuff
+		} else {
+			// put info that you're offline and can't tweet
+		}
+
+		// HTML5 LOCAL STORAGE
+		GAME.API.localStorage();
+
 		var str = GAME.$id('page-gameover').innerHTML;
 		str = str.replace('[0P]',GAME._points);
 		str = str.replace('[0%]',(GAME._points*5)); // *5, because /20 and *100 to have percents
 		str = str.replace('[0M]',~~(GAME._time/60));
 		str = str.replace('[0S]',(GAME._time%60));
-		GAME.$showModal('<div class="modal page">'+str+'</div>');
+		GAME.$showModal(str,'page');
 	}
 	else {
-		GAME.$showModal('<div class="modal page" id="page-'+page+'">'+GAME.$id('page-'+page).innerHTML+' <div class="continue"><span id="close">Close</span></div></div>');
+		GAME.$showModal(''+
+			GAME.$id('page-'+page).innerHTML+GAME.$txt.close,
+			'page',
+			'page-'+page);
 		GAME.$id('close').onclick = function() {
 			GAME.$hideModal();
 		}
--- js/init.js
+++ js/init.js
@@ -13,15 +13,94 @@ GAME.shuffleArray = function(array) {
  	}
 };
 
-GAME.$showModal = function(content) {
+GAME.$showModal = function(content,className,idName) {
+	var classHTML = (className) ? ' '+className : '',
+		idHTML = (idName) ? ' id="'+idName+'"' : '';
 	GAME.$id('formBg').style.display = 'block';
 	GAME.$id('modalBg').style.display = 'block';
 	if(content) {
-		GAME.$id('formBg').innerHTML = content;
+		GAME.$id('formBg').innerHTML = '<div class="modal'+classHTML+'"'+idHTML+'>'+content+'</div>';
 	}
 };
 
 GAME.$hideModal = function() {
 	GAME.$id('formBg').style.display = 'none';
 	GAME.$id('modalBg').style.display = 'none';
+};
+
+GAME.$form = function(item,question,answers) {
+	return "<div class='header'>"+
+			"<span class='photo' style='background-position: -"+(item.id*100)+"px 0;'></span>"+
+			"<span id='message'>What's the <strong>"+question+"</strong> of this person?</span>"+
+		"</div>"+
+		"<form id='form'>"+
+			"<p><input type='radio' name='q' value='"+answers[0]+"' id='radio1' /> <label id='radio1label' for='radio1'>"+answers[0]+"</label></p>"+
+			"<p><input type='radio' name='q' value='"+answers[1]+"' id='radio2' /> <label id='radio2label' for='radio2'>"+answers[1]+"</label></p>"+
+			"<p><input type='radio' name='q' value='"+answers[2]+"' id='radio3' /> <label id='radio3label' for='radio3'>"+answers[2]+"</label></p>"+
+		"</form>"+
+		"<div class='continue'><span id='continue'>Continue</span></div>";
+};
+
+GAME.$txt = {
+	'halfway': "<div>Nice, You're half way thru! You've managed to solve the first 10 pairs of photos, let's see how You'll handle the second part of the game.</div>",
+	'continue': "<div class='continue'><span id='newLevel'>Continue</span></div>",
+	'correct': "Congratulations, your answer is correct!",
+	'wrong': "Oh no, wrong answer, no points this time...",
+	'close': "<div class='continue'><span id='close'>Close</span></div>"
+};
+
+GAME.API = {};
+
+GAME.API.localStorage = function() {
+	try {
+		var localStorageSupport = 'localStorage' in window && window['localStorage'] !== null;
+	} catch (e) {
+		return false;
+	}
+	if(localStorageSupport) {
+		if(localStorage.getItem('frontface_points')) {
+			var bestPoints = localStorage.getItem('frontface_points');
+			if(GAME._points > bestPoints) { // congrats, you beat the score - new personal best
+				localStorage.setItem('frontface_points', GAME._points);
+				localStorage.setItem('frontface_trials', GAME._trials);
+				localStorage.setItem('frontface_time', GAME._time);
+				console.log('New personal best, congrats!');
+				// update html (and/or modal?)
+			}
+		}
+	}
+};
+
+GAME.API.offline = function() {
+	// offline
+};
+
+GAME.API.geolocation = function() {
+	if (navigator.geolocation) {
+		//console.log('Geolocation is supported!');
+
+		var startPos;
+		navigator.geolocation.getCurrentPosition(function(position) {
+			startPos = position;
+			GAME.$id('geo').innerHTML = '[ Lat: '+(~~startPos.coords.latitude)+' | Lon: '+(~~startPos.coords.longitude)+' ] Click 4 popup & more!';
+		});
+	}
+	else {
+		//console.log('Geolocation is not supported for this Browser/OS version yet.');
+	}
+
+	// calculat the distance between the Warsaw and the user's current location
+	// http://www.html5rocks.com/en/tutorials/geolocation/trip_meter/
+	// http://breakthebit.org/post/10512237123/using-html5-geolocation-api-to-get-the-distance-of-the
+	// http://mobile.tutsplus.com/tutorials/mobile-web-apps/html5-geolocation/
+	// http://www.ip2location.com/html5geolocationapi.aspx
+	// http://www.sitepoint.com/using-the-html5-geolocation-api/
+	// http://spektom.blogspot.com/2010/05/html5-geolocation-api-is-scaring-me.html
+	// http://robertnyman.com/2010/03/15/geolocation-in-web-browsers-to-find-location-google-maps-examples/
+	// !!! http://robertnyman.com/html5/geolocation/current-location-and-directions.html !!!
+	/*
+	document.getElementById('distance').innerHTML =
+		calculateDistance(startPos.coords.latitude, startPos.coords.longitude,
+			position.coords.latitude, position.coords.longitude);
+	*/
 };
